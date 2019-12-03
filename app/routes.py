982341539dbe658261d3cd8b2867e4d8f80c4a2c
Rory Harrison <40296172@live.napier.ac.uk>
2019-12-03 00:56:26 +0000

    Index Template and DataDragon

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
 from app import app
+import json
+import requests
+from forms import SummonerForm
+from flask import Flask, render_template, redirect, request, make_response
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    form = SummonerForm()
+    return render_template('index.html', form=form)
+
+@app.route('/summoner', methods=['GET', 'POST'])
+def get_data():
+    #Throws 400 if no args
+    summoner = request.args['summoner']
+    region = request.args['region']
+
+    data = json.loads(requests.get('https://www.masterypoints.com/api/v1.1/summoner/{}/{}'.format(summoner, region)).content)
+
+    return render_template('summoner.html', data=data)
+
+    
